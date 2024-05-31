158cfed9975acf1bbeb944fe0b4460c6e4319e9f
Marcus Reis <mvsantosreis@gmail.com>
2024-05-31 12:58:19 +0100

    Main Menu

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>Hello World</h1>
+  <MainLayout>
+    <div id="IndexPage" class="mt-4 max-w-[1200px] mx-auto px-2">IndexPage</div>
+  </MainLayout>
 </template>
 
-<script lang="ts" setup></script>
-
-<style></style>
+<script setup>
+import MainLayout from '~/layouts/MainLayout.vue'
+</script>
