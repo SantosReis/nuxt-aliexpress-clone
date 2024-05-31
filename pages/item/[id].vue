8d05c26be9eae386c2aadafc58741aa03daef62f
Marcus Reis <mvsantosreis@gmail.com>
2024-05-31 15:44:00 +0100

    Product Item Details

--- pages/item/[id].vue
+++ pages/item/[id].vue
@@ -17,6 +17,51 @@
             </div>
           </div>
         </div>
+        <div class="md:w-[60%] bg-white p-3 rounded-lg">
+          <div v-if="product && product.data">
+            <p class="mb-2">{{ product.data.title }}</p>
+            <p class="font-light text-[12px] mb-2">{{ product.data.description }}</p>
+          </div>
+
+          <div class="flex items-center pt-1.5">
+            <span class="h-4 min-w-4 rounded-full p-0.5 bg-[#FFD000] mr-2">
+              <Icon name="material-symbols:star-rounded" class="-mt-[17px]" size="12" />
+            </span>
+            <p class="text-[#FF5353]">Extra 5% off</p>
+          </div>
+
+          <div class="flex items-center justify-start my-2">
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <Icon name="ic:baseline-star" color="#FF5353" />
+            <span class="text-[13px] font-light ml-2">5 213 Reviews 1,000+ orders</span>
+          </div>
+
+          <div class="border-b" />
+
+          <div class="flex items-center justify-start gap-2 my-2">
+            <div class="text-xl font-bold">$ {{ priceComputed }}</div>
+            <span
+              class="bg-[#F5F5F5] border text-[#C08562] text-[9px] font-semibold px-1.5 rounded-sm"
+              >70% off</span
+            >
+          </div>
+
+          <p class="text-[#009A66] text-xs font-semibold pt-1">Free 11-day delivery over ￡8.28</p>
+
+          <p class="text-[#009A66] text-xs font-semibold pt-1">Free Shipping</p>
+
+          <div class="py-2" />
+
+          <button
+            class="px-6 py-2 rounded-lg text-white text-lg font-semibold bg-gradient-to-r from-[#FF851A] to-[#FFAC2C]"
+          >
+            <div v-if="isInCart">Is Added</div>
+            <div v-else>Add to Cart</div>
+          </button>
+        </div>
       </div>
     </div>
   </MainLayout>
@@ -25,7 +70,15 @@
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
 
+let product = ref(null)
 let currentImage = ref(null)
+
+const priceComputed = computed(() => {
+  if (product.value && product.value.data) {
+    return product.value.data.price / 100
+  }
+  return '0.00'
+})
 
 const images = ref([
   '',
