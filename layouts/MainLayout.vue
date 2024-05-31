374dc0a4b5fb7aca19f31f366e6848cbec3733b1
Marcus Reis <mvsantosreis@gmail.com>
2024-05-31 13:34:22 +0100

    Cart Menu

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -102,6 +102,27 @@
           </div>
         </div>
 
+        <NuxtLink to="/shoppingcart" class="flex items-center">
+          <button
+            class="relative md:block hidden"
+            @mouseenter="isCartHover = true"
+            @mouseleave="isCartHover = false"
+          >
+            <span
+              class="absolute flex items-center justify-center -right-[3px] top-0 bg-[#FF4646] h-[17px] min-w-[17px] text-xs text-white px-0.5 rounded-full"
+            >
+              0
+            </span>
+            <div class="min-w-[40px]">
+              <Icon
+                name="ph:shopping-cart-simple-light"
+                size="33"
+                :color="isCartHover ? '#FF4646' : ''"
+              />
+            </div>
+          </button>
+        </NuxtLink>
+
         <button class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200">
           <Icon name="radix-icons:hamburger-menu" size="33" />
         </button>
@@ -115,6 +136,7 @@
 
 <script setup>
 let isAccountMenu = ref(false)
+let isCartHover = ref(false)
 let isSearching = ref(false)
 let searchItem = ref('')
 </script>
