b8c0abca21af6b72eb1fbeeeaf76220f22c8e7d8
Marcus Reis <mvsantosreis@gmail.com>
2024-06-01 17:07:44 +0100

    Fetch Home Page Products

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,7 @@
       <div
         class="grid xl:grid-cols-6 lg:grid-cols-5 md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4"
       >
-        <div v-if="products" v-for="product in products" :key="product">
+        <div v-if="products" v-for="product in products.data" :key="product">
           <ProductComponent :product="product" />
         </div>
       </div>
@@ -14,50 +14,12 @@
 
 <script setup>
 import MainLayout from '~/layouts/MainLayout.vue'
+import { useUserStore } from '~/stores/user'
+const userStore = useUserStore()
 
-const products = [
-  {
-    id: 1,
-    title: 'Title 1',
-    description: 'This is a description',
-    url: 'https://picsum.photos/id/7/800/800',
-    price: 9899
-  },
-  {
-    id: 2,
-    title: 'Title 1',
-    description: 'This is a description',
-    url: 'https://picsum.photos/id/71/800/800',
-    price: 9699
-  },
-  {
-    id: 3,
-    title: 'Title 1',
-    description: 'This is a description',
-    url: 'https://picsum.photos/id/72/800/800',
-    price: 29999
-  },
-  {
-    id: 4,
-    title: 'Title 1',
-    description: 'This is a description',
-    url: 'https://picsum.photos/id/73/800/800',
-    price: 9699
-  },
-  {
-    id: 5,
-    title: 'Title 1',
-    description: 'This is a description',
-    url: 'https://picsum.photos/id/74/800/800',
-    price: 94999
-  }
-]
-// import { useUserStore } from '~/stores/user'
-// const userStore = useUserStore()
-
-// let products = ref(null)
-// onBeforeMount(async () => {
-//   products.value = await useFetch('/api/prisma/get-all-products')
-//   setTimeout(() => (userStore.isLoading = false), 1000)
-// })
+let products = ref(null)
+onBeforeMount(async () => {
+  products.value = await useFetch('/api/prisma/get-all-products')
+  setTimeout(() => (userStore.isLoading = false), 1000)
+})
 </script>
