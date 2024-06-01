3743b1fa0c5e15b8df5e869b2e0c9b13cfb35c6c
Marcus Reis <mvsantosreis@gmail.com>
2024-06-01 17:26:01 +0100

    Signout option

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -37,7 +37,7 @@
             v-if="isAccountMenu"
             class="absolute bg-white w-[220px] text-[#333333] z-40 top-[38px] -left-[100px] border-x border-b"
           >
-            <div v-if="true">
+            <div v-if="!user">
               <div class="text-semibold text-[15px] my-4 px-3">Welcome to AliExpress!</div>
               <div class="flex items-center gap-1 px-3 mb-3">
                 <NuxtLink
@@ -56,7 +56,13 @@
               >
                 My Orders
               </li>
-              <li v-if="true" class="text-[13px] py-2 px-4 w-full hover:bg-gray-200">Sign out</li>
+              <li
+                v-if="user"
+                @click="client.auth.signOut()"
+                class="text-[13px] py-2 px-4 w-full hover:bg-gray-200"
+              >
+                Sign out
+              </li>
             </ul>
           </div>
         </li>
@@ -145,8 +151,8 @@
 import { useUserStore } from '~/stores/user'
 const userStore = useUserStore()
 
-// const client = useSupabaseClient()
-// const user = useSupabaseUser()
+const client = useSupabaseClient()
+const user = useSupabaseUser()
 
 let isAccountMenu = ref(false)
 let isCartHover = ref(false)
