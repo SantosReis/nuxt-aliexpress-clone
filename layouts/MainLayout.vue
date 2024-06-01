61284b7c7b5d56e0142b452d77c87c7b6f1186a1
Marcus Reis <mvsantosreis@gmail.com>
2024-06-01 10:17:29 +0100

    Mobile Menu

--- layouts/MainLayout.vue
+++ layouts/MainLayout.vue
@@ -123,7 +123,10 @@
           </button>
         </NuxtLink>
 
-        <button class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200">
+        <button
+          @click="userStore.isMenuOverlay = true"
+          class="md:hidden block rounded-full p-1.5 -mt-[4px] hover:bg-gray-200"
+        >
           <Icon name="radix-icons:hamburger-menu" size="33" />
         </button>
       </div>
